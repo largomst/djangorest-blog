fa7b8846c96d91a91c83b6934ad0712f19180566
largomst <dingrui.zhen@gmail.com>
2021-10-25 16:16:22 +0800

    replace APIView to generics

--- article/views.py
+++ article/views.py
@@ -4,58 +4,18 @@ from article.models import Article
 from article.serializers import ArticleSerializer, ArticleDetailSerializer
 
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 
 # Create your views here.
 
 
-class ArticleDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Article.objects.get(pk=pk)
-        except:
-            raise Http404
-
-    def get(self, request, pk):
-        article = self.get_object(pk)
-        print(article)
-        serializer = ArticleDetailSerializer(article)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleDetailSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        article = self.get_object(pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def patch(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleDetailSerializer(
-            article, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
 
 
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
