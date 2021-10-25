61ce30d2331f8d3fd382a4fe63ec0b837ca07d5a
largomst <dingrui.zhen@gmail.com>
2021-10-25 15:47:13 +0800

    replace Serializer to ModelSerializer

--- article/serializers.py
+++ article/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
+from article.models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(allow_blank=False, max_length=100)
-    body = serializers.CharField(allow_blank=True)
-    created = serializers.UniqueForDateValidator
-    updated = serializers.UniqueForDateValidator
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = (
+            'id', 'title', 'body', 'created'
+        )
