4afdab278a03a9f7c0beebc1e4f208cd4ce546c4
largomst <dingrui.zhen@gmail.com>
2021-10-25 15:47:35 +0800

    use @api_view

--- article/views.py
+++ article/views.py
@@ -1,12 +1,22 @@
-from django.http import JsonResponse
 from article.models import Article
 from article.serializers import ArticleSerializer
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
